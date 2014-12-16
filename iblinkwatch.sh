42dade0e37ab951f2b512460bd08627d4f14cbf5
Kurt J. Strosahl <strosahl@jlab.org>
2014-12-16 08:43:36 -0500

    Mostly done, just needs full testing 	modified:   iblinkwatch.sh

--- iblinkwatch.sh
+++ iblinkwatch.sh
@@ -6,18 +6,21 @@
 
 #email address for notifications to be sent
 EMAIL="strosahl@jlab.org"
-MUTT="/usr/bin/mutt"
+MUTT='/usr/bin/mutt'
 
 #primary bond
 PRIMARY="ib1"
 
-TIMELIMIT=5
+TIMELIMIT=1
 
 #active string
 ACTIVE="Currently Active Slave"
 
 #file created to tell script that the error has already been detected
+TOUCH="/bin/touch"
 TOUCHFILE="ibalert"
+
+INFO="/proc/net/bonding/bond0"
 
 #information on current bond status
 CURRENT=`cat /proc/net/bonding/bond0 | grep "$ACTIVE"`
@@ -29,23 +32,30 @@ if [[ $CURRENT == *"$PRIMARY"* ]]
 then
 	if [[ -f $TOUCHFILE ]]
 	then
+		$MUTT -s "IB link restored to $PRIMARY on $HOSTNAME" $EMAIL < $INFO
 		rm $TOUCHFILE
 	fi
-	echo "primary is correct, exiting"
-#commented out for testing
-#	exit	
+	exit	
 fi
 
 #checks to see if a notification has already been given in the last 2 hours
 #if the touch file exists and is over a certain age
-ALERT=`find $TOUCHFILE -mmin +$TIMELIMIT -exec echo "NOTIFIED" \;`
+if [[ -f $TOUCHFILE ]]
+then
+	ALERT=`find $TOUCHFILE -mmin +$TIMELIMIT -exec echo "NOTIFIED" \;`
+else
+	ALERT="NO"
+fi
 
 if [[ $ALERT == "NOTIFIED" ]]
 then
 	echo "notification sent, exiting"
-#commented out for testing
-#	exit
+	exit
 fi
 
+#at this point a notification needs to be sent
+$MUTT -s "ALERT IB link status change on $HOSTNAME" $EMAIL < $INFO
 
-echo "end"
+#create touchfile
+
+$TOUCH $TOUCHFILE
